3cf9c8ee79707ec65e44c21c1db6dc683b344eb3
Connor P Bourque <47765857+CPBPILOT@users.noreply.github.com>
2023-02-03 16:36:11 -0600

    Update pam.py

--- klippy_extra/pam.py
+++ klippy_extra/pam.py
@@ -49,7 +49,7 @@ class PAM:
             mesh_cy = min(6, mesh_cy)
         # if self.optimus_prime == True:
         #     self.set_priming_location(mesh_x0, mesh_y0, mesh_x1, mesh_y1)
-        self.gcode.respond_raw("PAM v0.1.7.3 bed mesh leveling...")
+        self.gcode.respond_raw("PAM v0.1.7.4 bed mesh leveling...")
         self.gcode.respond_raw("This is a test")
         self.gcode.run_script_from_command('BED_MESH_CALIBRATE PROFILE=ratos mesh_min={0},{1} mesh_max={2},{3} probe_count={4},{5} relative_reference_index=-1'.format(mesh_x0, mesh_y0, mesh_x1, mesh_y1, mesh_cx, mesh_cy))
 
